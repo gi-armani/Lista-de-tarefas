c31980ae4f9cadb16a62cc786b58a5ba43dbeacb
gi-armani <giovana_armani@usp.br>
2020-11-08 22:41:27 -0300

    Adicao de menu de navegacao

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <header style={headerStyle} >
             <h1>Lista de Tarefas</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -13,6 +15,11 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '10px'
+}
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header;
