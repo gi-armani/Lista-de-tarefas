4412255a479a5094680953ab32b708b0c39b438f
gi-armani <giovana_armani@usp.br>
2020-11-08 21:51:35 -0300

    Adicao de action para criar uma nova tarefa ao submeter o formulario

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import './App.css';
 import Header from './components/layouts/Header';
 import AdicionaTarefa from './components/AdicionaTarefa';
 import Tarefas from './components/Tarefas';
+import {v4 as uuid} from 'uuid';
 
 class App extends Component {
 
   state = {
     tarefas: [
       {
-        id: 1,
+        id: uuid(),
         title: 'Fazer curso de React',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Preparar aulas de Arduino',
         completed: false
       },
       {
-        id: 3,
+        id: uuid(),
         title: 'Passar aspirador de pó',
         completed: false
       }
     ]
   }
 
+  //Marcar tarefa como completa (risca ela na lista)
   markComplete = (id) => {
     this.setState( {tarefas: this.state.tarefas.map(tarefa => {
       if(tarefa.id === id) {
@@ -35,8 +37,18 @@ class App extends Component {
     })} )
   }
 
+  //Deletar tarefa
   delTarefa = (id) => {
     this.setState({tarefas: [...this.state.tarefas.filter(tarefa => tarefa.id !== id)]});
+  }
+
+  adicionaTarefa = (title) => {
+    const novaTarefa = {
+      id: uuid(),
+      title: title,
+      completed: false
+    }
+    this.setState({ tarefas: [...this.state.tarefas, novaTarefa] });
   }
 
   render() {
@@ -44,7 +56,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
         <Header />
-        <AdicionaTarefa />
+        <AdicionaTarefa adicionaTarefa={this.adicionaTarefa} />
         <Tarefas tarefas={this.state.tarefas} markComplete={this.markComplete} delTarefa={this.delTarefa} />
         </div>
       </div>
--- src/components/AdicionaTarefa.js
+++ src/components/AdicionaTarefa.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 class AdicionaTarefa extends Component {
+
+    state = {
+        title: ''
+    }
+
+    onFillForm = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        this.props.adicionaTarefa(this.state.title);
+        this.setState({ title: '' });
+    }
+
     render() {
         return(
-            <form style={{display: 'flex'}}>
+            <form onSubmit={this.onSubmitForm} style={{display: 'flex'}}>
                 <input 
                     type='text' 
                     name='title' 
                     placeholder='Adicionar tarefa...' 
                     style={{flex: '10', padding: '5px'}}
+                    value={this.state.title}
+                    onChange={this.onFillForm}
                 />
                 <input 
                     type='submit'
