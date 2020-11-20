901a6a6c1dd89795ef3273c88fe7958b968a9452
gi-armani <giovana_armani@usp.br>
2020-11-20 08:19:29 -0300

    Ajuste de erro na funcao de inserir tarefas no banco

--- server/src/controllers/index.controllers.js
+++ server/src/controllers/index.controllers.js
@@ -15,16 +15,17 @@ const getTarefas = async (req, res) => {
 }
 
 const createTarefas = async (req, res) => {
-    const nome = req.body.nome;
-    const categoria = req.body.categoria;
+    let nome = req.body.nome;
+    let categoria = req.body.categoria;
 
     //const query = "INSERT INTO tarefas (id, nome, categoria) VALUES (2, 'Estudar banco de dados', 'Graduação');";
-    const query = "INSERT INTO tarefas (nome, categoria) VALUES (?, ?);";
-    const response = await pool.query(query, [nome, categoria], (err, result) => {
+    const query = "INSERT INTO tarefas (nome, categoria) VALUES ('" + nome + "', '" + categoria + "')";
+    const response = await pool.query(query, (err, result) => {
         if(err) {
             console.log(err)
         } else {
             res.send("Values Inserted")
+            console.log("Show")
         }
     });
     //res.status(200).json(response.rows);
